f9eed9471057ab2e5f71f905c6bf6841379f55fe
Marcos Martinez <marcos.martinez@gmail.com>
2020-02-11 19:20:28 +0100

    Java client/server operational

--- Java/JavaTCPClient/sources/JavaTCPClient.java
+++ Java/JavaTCPClient/sources/JavaTCPClient.java
@@ -1,6 +1,11 @@
-/* *********************** */
-/* *** JAVA TCP CLIENT *** */
-/* *********************** */
+/* ****************************** */
+/* *** JAVA TCP CLIENT        *** */
+/* ****************************** */
+/* *** This client sends TLVs *** */
+/* ***   Type: 1 byte         *** */
+/* ***   Length: 1 byte       *** */
+/* ***   Value: N bytes       *** */
+/* ****************************** */
 import java.io.*;
 import java.net.*;
 
@@ -17,11 +22,30 @@ class JavaTCPClient {
 	Socket clientSocket = new Socket("localhost", 6789);
 	DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 	BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+	// Get the data to send
 	sentence = inFromUser.readLine();
-	System.out.println("Sending: " + sentence);
-	outToServer.writeBytes(sentence + '\n');
-	modifiedSentence = inFromServer.readLine();
-	System.out.println("FROM SERVER: " + modifiedSentence);
+	System.out.println("Preparing to send: " + sentence);
+
+	// Get the sentence and chop it into bytes
+	byte[] b = sentence.getBytes("UTF8");
+	System.out.println("LENGTH: " + b.length);
+	for (int i = 0; i<b.length; i++)
+	    {
+		System.out.println("\tBYTE[" + i + "]: " + b[i]);
+	    }
+
+	// Send the type field
+	outToServer.writeByte((byte)1);
+	// Send the length field
+	outToServer.writeByte((byte)b.length);
+	// Send the data
+	outToServer.write(b,0,b.length);
+
+	
+	
+	//modifiedSentence = inFromServer.readLine();
+	//System.out.println("FROM SERVER: " + modifiedSentence);
 	clientSocket.close();
     }
 }
--- Java/JavaTCPServer/sources/JavaTCPServer.java
+++ Java/JavaTCPServer/sources/JavaTCPServer.java
@@ -1,8 +1,15 @@
-/* *********************** */
-/* *** JAVA TCP SERVER *** */
-/* *********************** */
+/* ****************************** */
+/* *** JAVA TCP SERVER        *** */
+/* ****************************** */
+/* *** This server reads TLVs *** */
+/* ***   Type: 1 byte         *** */
+/* ***   Length: 1 byte       *** */
+/* ***   Value: N bytes       *** */
+/* ****************************** */
 import java.io.*;
 import java.net.*;
+
+enum FSM_State {TYPE,LENGTH,VALUE};
 
 /* ************ */
 /* *** MAIN *** */
@@ -11,12 +18,21 @@ class JavaTCPServer {
  public static void main(String argv[]) throws Exception {
 
      // Variables
+     int TLVType = 0;
+     int TLVLength = 0;
+     boolean end = false;
+     
      String clientSentence;
      String capitalizedSentence;
      ServerSocket welcomeSocket = new ServerSocket(6789);
+     int count;
 
      // Main loop
      while (true) {
+
+	 // Create a value field
+	 byte[] bytes = new byte[1024];
+	 FSM_State FSM = FSM_State.TYPE;
 
 	 // Accept the socket
 	 Socket connectionSocket = welcomeSocket.accept();
@@ -24,13 +40,58 @@ class JavaTCPServer {
 	 System.out.println(" THE CLIENT" + " " + connectionSocket.getInetAddress()
 			    + ":" + connectionSocket.getPort() + " IS CONNECTED ");
 	 // read the data
-	 BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-	 clientSentence = inFromClient.readLine();
-	 System.out.println("Received: " + clientSentence);
+	 DataInputStream in = new DataInputStream(new BufferedInputStream(connectionSocket.getInputStream()));
+	 //BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+	 //while ((count = inFromClient.read()) >0);
+	 //{
+	 //clientSentence = inFromClient.readLine();
+	 //clientSentence = inFromClient.readChar();
+	 //System.out.println("Received: " + clientSentence);
+        // and on the main thread we read from the socket and write
+        // the contents to the console.
+	 byte[] TLV;
+	 while (!end)
+	     {
+		 switch(FSM)
+		     {
+		     case TYPE:
+			 TLVType = (int)in.readByte();
+			 FSM = FSM_State.LENGTH;
+			 System.out.println("TYPE: " + TLVType);
+			 break;
+		     case LENGTH:
+			 TLVLength = in.readByte();
+			 FSM = FSM_State.VALUE;
+			 System.out.println("LENGTH: " + (int)TLVLength);
+			 break;
+		     case VALUE:
+			 int BytesRead = 0;
+			 TLV = new byte[TLVLength];
+			 while( BytesRead < TLVLength) {
+         
+			     // read one single byte
+			     TLV[BytesRead]= in.readByte();
+			     //String A = Byte.toString(in.readByte());
+			     System.out.println("b[" + BytesRead + "]: " + TLV[BytesRead]);
+			     BytesRead++;
+			 }
+			 FSM = FSM_State.TYPE;
+			 break;
+		     }
+	     }
+	 
 
-	 DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-	 capitalizedSentence = clientSentence.toUpperCase() + '\n';
-	 outToClient.writeBytes(capitalizedSentence);
+	 //byte[] bytes = new byte[1024];
+	 // int len;
+        	 //while((len = in.read(bytes)) > 0) {
+        	 //    String echoStr = new String(bytes, 0, len);
+        	 //    System.out.println("From Client:" + echoStr);
+        	 //}
+	 //}	     
+
+	 // DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+	 	 //capitalizedSentence = clientSentence.toUpperCase() + '\n';
+	 	 //outToClient.writeBytes(capitalizedSentence);
      }
  }
 }
