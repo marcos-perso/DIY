b527e5e6aa661c19aff143c9a2423bc7a6b90966
Marcos Martinez <marcos.martinez@gmail.com>
2020-06-01 00:46:08 +0200

    MQTT communication is working

--- Arduino-PIO-Prjs/Prjs/PlantuinoII/src/main.cpp
+++ Arduino-PIO-Prjs/Prjs/PlantuinoII/src/main.cpp
@@ -3,7 +3,7 @@
 // ################
 #include <Arduino.h>
 #include "WiFi.h"
-#include "HardwareSerial.h"
+#include "PubSubClient.h"
 
 // ###################
 // ### Description ###
@@ -28,11 +28,13 @@
 // #################
 const char* ssid = "YM-IoT";
 const char* password = "Zumarraga0001";
+const char* mqtt_server = "192.168.8.103";
 
 // ###############
 // ### OBJECTS ###
 // ###############
-//HardwareSerial Serial2(2);
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 
 // #################
@@ -54,7 +56,11 @@ String translateEncryptionType(wifi_auth_mode_t encryptionType) {
       return "WPA_WPA2_PSK";
     case (WIFI_AUTH_WPA2_ENTERPRISE):
       return "WPA2_ENTERPRISE";
+    case (WIFI_AUTH_MAX):
+      return "TBD";
   }
+
+  return "Done";
 }
  
 void scanNetworks() {
@@ -95,6 +101,58 @@ void connectToNetwork() {
  
 }
 
+// Callback when receiving MQTT topic
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial2.print("Message arrived on topic: ");
+  Serial2.print(topic);
+  Serial2.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial2.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial2.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "Marcos") {
+
+    Serial2.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial2.println("on");
+      digitalWrite(LED_PIN, HIGH);
+    }
+    else if(messageTemp == "off"){
+      Serial2.println("off");
+      digitalWrite(LED_PIN, LOW);
+    }
+  }
+}
+
+// Reconnect to MQTT broker
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial2.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP32Client")) {
+      Serial2.println("connected");
+      // Subscribe
+      client.subscribe("Marcos");
+    } else {
+      Serial2.print("failed, rc=");
+      Serial2.print(client.state());
+      Serial2.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 // #############
 // ### SETUP ###
 // #############
@@ -113,6 +171,9 @@ void setup() {
  
   Serial2.println(WiFi.macAddress());
   Serial2.println(WiFi.localIP());
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
  
  
 }
@@ -123,9 +184,17 @@ void setup() {
  
 void loop() {
 
-  Serial2.println(F("Marcos1"));
-  digitalWrite(LED_PIN,HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN,LOW);
-  delay(1000);
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+ // Serial2.println(F("Marcos1"));
+//  digitalWrite(LED_PIN,HIGH);
+//  delay(1000);
+//  digitalWrite(LED_PIN,LOW);
+//  delay(1000);
+
+//  client.publish("home", "Marcos");
+
 }
